3ab6b41124641e0076501ff7784fbca29e1991f5
Vasudha-01 <120655215+Vasudha-01@users.noreply.github.com>
2024-05-09 12:36:33 +0530

    Update DS-P1.py

--- DS-P1.py
+++ DS-P1.py
@@ -7,6 +7,6 @@ hours_studied=[10,9,2,15,10,16,11,16]
 exam_scores=[95,80,10,50,45,98,38,93]
 plt.plot(hours_studied,exam_scores,marker='*',color='red',linestyle='-')
 plt.xlabel('Hours Studied')
-plt.ylabel=('Score in Final Exam')
+plt.ylabel('Score in Final Exam')
 plt.title('Effect of Hours Studied on Exam Score')
 plt.show()
