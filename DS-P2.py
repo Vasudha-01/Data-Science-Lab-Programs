f851c5a3b7f803b2197f96a2d31d6bef65aec424
Vasudha-01 <120655215+Vasudha-01@users.noreply.github.com>
2024-05-16 11:59:19 +0530

    Add files via upload

---
+++ DS-P2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+mtcars = pd.read_csv("C:\\Users\\CSD 29\\Downloads\\mtcars.csv")
+
+
+# In[5]:
+
+
+plt.hist(mtcars['mpg'], bins=10, color='blue', edgecolor='black')
+plt.xlabel('Miles per gallon (mpg)')
+plt.ylabel('Frequency')
+plt.title('Histogram of Miles per gallon (mpg)')
+plt.show()
+
