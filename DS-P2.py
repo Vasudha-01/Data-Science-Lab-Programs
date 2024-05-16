4c02b6175151281b82f1c876efbbd8e47b1af3ad
Vasudha-01 <120655215+Vasudha-01@users.noreply.github.com>
2024-05-16 12:59:26 +0530

    Update DS-P2.py

--- DS-P2.py
+++ DS-P2.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# In[2]:
+!dir mtc*
+mtcars = pd.read_csv("mtcars.csv")
 
-mtcars = pd.read_csv("C:\\Users\\CSD 29\\Downloads\\mtcars.csv")
 mtcars.head()
 mtcars.shape
 mtcars.info()
 mtcars.columns
 mtcars.describe()
 
-# In[5]:
-
-
 plt.hist(mtcars['mpg'], bins=10, color='blue', edgecolor='black')
 plt.xlabel('Miles per gallon (mpg)')
 plt.ylabel('Frequency')
 plt.title('Histogram of Miles per gallon (mpg)')
 plt.show()
-
